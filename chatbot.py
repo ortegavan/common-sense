abd011ba672c2f9d6cf42f5ff2fae051312c9751
Vanessa Ortega <ortegavan@live.com>
2026-05-17 16:49:09 -0300

    feat: implementa chatbot

--- chatbot.py
+++ chatbot.py
@@ -4,10 +4,26 @@ from openai import OpenAI
 load_dotenv()
 llm = OpenAI()
 
-response = llm.responses.create(
-    model="gpt-5.4-mini",
-    temperature=0.5,
-    input="Quem é a mulher mais importante da história da computação?"
-)
+assistant_message = "Pergunte-me algo." 
+print(f"Assistente: {assistant_message}\n")
+user_message = input("Usuário: ")
+history = [
+    {"role": "developer", "content": "Você é um professor, escritor e divulgador da ciência especialista em física que sempre responde de forma clara, objetiva e didática, utilizando exemplos e analogias para facilitar a compreensão dos conceitos, alguém como Marcelo Leite."},
+    {"role": "assistant", "content": assistant_message},
+    {"role": "user", "content": user_message}
+]
 
-print(response.output_text)
+while user_message != "sair":
+    response = llm.responses.create(
+        model="gpt-5.4-mini",
+        temperature=0.5,
+        input=history
+    )
+    
+    print(f"\nAssistente: {response.output_text}")
+    user_message = input("\nUsuário: ")
+
+    history += [
+        {"role": "assistant", "content": response.output_text},
+        {"role": "user", "content": user_message}
+    ]
